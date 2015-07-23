e12a7b3936a3046128ea4ec5187ea68b70f8b2ad
ChefDK User <chef@ip-172-31-60-184.ec2.internal>
2015-07-23 17:20:42 +0000

    Version 0.2.0 - Added ip info and hostname info to index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,7 +2,10 @@ package "apache2"
 
 file "/var/www/html/index.html" do
   action :create
-  content "<h1>Hello, world!</h1>"
+  content "<h1>Hello, world!</h1>
+  <h3>IP ADDRESS: #{node["ipaddress"]}</h3>
+  <h3>Hostname  : #{node["hostname"]}</h3>
+  "
 end
 
 service "apache2" do
