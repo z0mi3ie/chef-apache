049e38562cae782b9ebeb1e0ed35c1b65e68b3be
ChefDK User <chef@ip-172-31-60-184.ec2.internal>
2015-07-23 16:02:56 +0000

    Added Apache cookbook tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,22 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe command('curl http://localhost') do
+    its(:stdout) { should match /<h1>Hello, world!<\/h1>/ }
+    its(:stdout) { should_not match /ra ra ra/ }
   end
+
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  describe port(81) do
+    it { should_not be_listening }
+  end 
+
+  describe file("/var/www/html/index.html") do
+    it { should exist }
+  end
+
 end
